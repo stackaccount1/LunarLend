2606c27ed4f0f78a421f58da5eb7988c5a98721a
Ira <iraf333@gmail.com>
2022-10-06 23:47:00 -0400

    smart contract updates to main contract weth logic

---
+++ hardhat-starter-kit/contracts/LunarMain.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+// 3. Interfaces, Libraries, Contracts, Errors
+//error error_notenougheth();
+error Not_Owner();
+
+/**@title Lunar Lend Main Contract
+ * @author Stackaccount1
+ * @notice This is a contract to deposit ETH and be returned WETH, a chainlink VRF counts time,
+ * @dev This implements price feeds as our library
+ */
+
+//WETH Contract Deposit ETH, Able to withdraw WETH, Upon Withdraw Account Is In Borrow Mode Account Reduced at 8% per year fee,
+//  No liquidation function ETH TO ETH  ust return Weth to Eth position will be reduced, Goverance token printed to borrowers and lenders,
+//  Later -> Deposit funds, choose lend func, get 3.5% per year to match max of 3.5% pool or less
+
+// Steps to Complete:
+//WETH Working correctly as ERC20
+//Chainlink VRF Working Correctly as Mock to deduct balance,
+//Distribute Litquidity token seperate ERC20 into the contract
+
+//constructor
+//Type Declarations
+// *price converter
+//State Variables
+//Events
+//Modifiers
+//Functions
+///constructor
+//recieve
+//fallback
+//external
+//public
+
+//WETH9
+
+contract LunarLend {
+    string public name = "Wrapped Ether";
+    string public symbol = "WETH";
+    uint8 public decimals = 18;
+
+    event Approval(address indexed src, address indexed guy, uint256 wad);
+    event Transfer(address indexed src, address indexed dst, uint256 wad);
+    event Deposit(address indexed dst, uint256 wad);
+    event Withdrawal(address indexed src, uint256 wad);
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    receive() external payable {
+        deposit();
+    }
+
+    function deposit() public payable {
+        balanceOf[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw(uint256 wad) public {
+        require(balanceOf[msg.sender] >= wad);
+        balanceOf[msg.sender] -= wad;
+        payable(msg.sender).transfer(wad);
+        emit Withdrawal(msg.sender, wad);
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function approve(address guy, uint256 wad) public returns (bool) {
+        allowance[msg.sender][guy] = wad;
+        emit Approval(msg.sender, guy, wad);
+        return true;
+    }
+
+    function transfer(address dst, uint256 wad) public returns (bool) {
+        return transferFrom(msg.sender, dst, wad);
+    }
+
+    function transferFrom(
+        address src,
+        address dst,
+        uint256 wad
+    ) public returns (bool) {
+        require(balanceOf[src] >= wad);
+
+        if (src != msg.sender && allowance[src][msg.sender] != type(uint256).max) {
+            require(allowance[src][msg.sender] >= wad);
+            allowance[src][msg.sender] -= wad;
+        }
+
+        balanceOf[src] -= wad;
+        balanceOf[dst] += wad;
+
+        emit Transfer(src, dst, wad);
+
+        return true;
+    }
+}
